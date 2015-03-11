68a8acf7e0c0247e9a97fbfa42f791102af6815f
jupitter <rds_wang@hotmail.com>
2015-03-11 09:13:46 +0100

    Create spontal_filter.py

---
+++ spontal_filter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#Remy Wang
+import sys
+import xml.etree.ElementTree as ET
+
+
+def main(argv):
+	tree = ET.parse(argv[1])
+	root = tree.getroot()
+	
+	for child in root.findall('POINT'):
+		a = float(child.find('BOTTOM_HZ').text)
+		b = float(child.find('TOP_HZ').text)
+		c = float(child.find('F0_END').text)
+		d = float(child.find('F0_START').text)
+		if c < a or d > b :
+			root.remove(child)
+		
+	
+	tree.write(argv[2])
+	
+
+	
+	
+main(sys.argv)	
+	
+	
+	
+	
+	
+
